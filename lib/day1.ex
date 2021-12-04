7bbf16f3fcf04c6bb06e7046b7fa066ef4c84470
Tejas Bubane <tejasbubane@gmail.com>
2021-12-05 01:48:04 +0530

    Solve day 1 - part 2

--- lib/day1.ex
+++ lib/day1.ex
@@ -4,4 +4,8 @@ defmodule Day1 do
     |> Enum.chunk_every(2, 1, :discard)
     |> Enum.count(fn [a, b] -> a < b end)
   end
+
+  def window_sum_increments(list) do
+    list |> Enum.chunk_every(3, 1, :discard) |> Enum.map(&Enum.sum/1) |> increments()
+  end
 end
--- test/day1_test.exs
+++ test/day1_test.exs
@@ -1,11 +1,19 @@
 defmodule Day1Test do
   use ExUnit.Case
 
-  test "increments - works for give example" do
+  test "part 1 - works for given example" do
     assert Day1.increments([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]) == 7
   end
 
-  test "increments - finds solution" do
+  test "part 1 - finds solution" do
     assert Day1.increments(Parser.lines_to_list("inputs/day1")) == 1288
+  end
+
+  test "part 2 - works for given example" do
+    assert Day1.window_sum_increments([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]) == 5
+  end
+
+  test "part 2 - finds solution" do
+    assert Day1.window_sum_increments(Parser.lines_to_list("inputs/day1")) == 1311
   end
 end
