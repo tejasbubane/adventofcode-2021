20672dc9b316f60568d1b571b696931c7d59bff6
Tejas Bubane <tejasbubane@gmail.com>
2021-12-05 21:52:53 +0530

    Solve day 3 - part 2

--- lib/day3.ex
+++ lib/day3.ex
@@ -1,26 +1,51 @@
 defmodule Day3 do
+  # Part 1
   def power_consumption(report) do
-    frequencies =
-      report
-      |> Enum.zip_with(& &1)
-      |> Enum.map(&Enum.frequencies/1)
-
-    calculate(:most, frequencies) * calculate(:least, frequencies)
+    integer_of(common_bits(report, :most)) * integer_of(common_bits(report, :least))
   end
 
-  defp calculate(strategy, frequencies) do
-    frequencies
-    |> Enum.map(&common_bit(strategy, &1))
-    |> Enum.join()
-    |> Integer.parse(2)
-    |> elem(0)
+  # Part 2
+  def life_support_rating(report, size) do
+    oxygen_rating(report, size) * co2_scrubber_rating(report, size)
+  end
+
+  def oxygen_rating(report, size), do: report |> rating(:most, size)
+
+  def co2_scrubber_rating(report, size), do: report |> rating(:least, size)
+
+  # Private functions
+  defp rating(report, strategy, bit_length) do
+    Enum.to_list(0..bit_length)
+    |> Enum.reduce(report, fn i, acc -> filter(acc, i, strategy) end)
+    |> List.first()
+    |> integer_of()
+  end
+
+  # Filters if list has more than one elements
+  defp filter([x], _, _), do: [x]
+
+  defp filter(report, index, strategy) do
+    bits = report |> common_bits(strategy) |> List.to_tuple()
+    report |> Enum.filter(fn item -> elem(List.to_tuple(item), index) == elem(bits, index) end)
+  end
+
+  defp integer_of(common_bits) do
+    common_bits |> Enum.join() |> Integer.parse(2) |> elem(0)
+  end
+
+  def common_bits(report, strategy) do
+    report |> transpose() |> Enum.map(&Enum.frequencies/1) |> Enum.map(&common_bit(strategy, &1))
+  end
+
+  defp transpose(list_of_lists) do
+    list_of_lists |> Enum.zip_with(& &1)
   end
 
   defp common_bit(:most, map) do
-    if map[0] > map[1], do: 0, else: 1
+    if map[1] >= map[0], do: 1, else: 0
   end
 
   defp common_bit(:least, map) do
-    if map[0] > map[1], do: 1, else: 0
+    if map[0] <= map[1], do: 0, else: 1
   end
 end
--- test/day3_test.exs
+++ test/day3_test.exs
@@ -2,30 +2,51 @@ defmodule Day3Test do
   use ExUnit.Case
   import Parser
 
-  def parse_report(lines) do
-    lines
-    |> Enum.map(&String.split(&1, "", trim: true))
-    |> Enum.map(fn a -> a |> Enum.map(&String.to_integer/1) end)
+  setup do
+    example_input = [
+      [0, 0, 1, 0, 0],
+      [1, 1, 1, 1, 0],
+      [1, 0, 1, 1, 0],
+      [1, 0, 1, 1, 1],
+      [1, 0, 1, 0, 1],
+      [0, 1, 1, 1, 1],
+      [0, 0, 1, 1, 1],
+      [1, 1, 1, 0, 0],
+      [1, 0, 0, 0, 0],
+      [1, 1, 0, 0, 1],
+      [0, 0, 0, 1, 0],
+      [0, 1, 0, 1, 0]
+    ]
+
+    puzzle_input =
+      lines_to_list("inputs/day3")
+      |> Enum.map(&String.split(&1, "", trim: true))
+      |> Enum.map(fn a -> a |> Enum.map(&String.to_integer/1) end)
+
+    {:ok, example_input: example_input, puzzle_input: puzzle_input}
   end
 
-  test "part 1 - works for given example" do
-    assert Day3.power_consumption([
-             [0, 0, 1, 0, 0],
-             [1, 1, 1, 1, 0],
-             [1, 0, 1, 1, 0],
-             [1, 0, 1, 1, 1],
-             [1, 0, 1, 0, 1],
-             [0, 1, 1, 1, 1],
-             [0, 0, 1, 1, 1],
-             [1, 1, 1, 0, 0],
-             [1, 0, 0, 0, 0],
-             [1, 1, 0, 0, 1],
-             [0, 0, 0, 1, 0],
-             [0, 1, 0, 1, 0]
-           ]) == 198
+  test "part 1 - works for given example", context do
+    assert Day3.power_consumption(context[:example_input]) == 198
   end
 
-  test "part 1 - solves the puzzle" do
-    assert Day3.power_consumption(lines_to_list("inputs/day3") |> parse_report) == 852_500
+  test "part 1 - solves the puzzle", context do
+    assert Day3.power_consumption(context[:puzzle_input]) == 852_500
+  end
+
+  test "oxygen_rating - works for given example", context do
+    assert Day3.oxygen_rating(context[:example_input], 4) == 23
+  end
+
+  test "co2_scrubber_rating - works for given example", context do
+    assert Day3.co2_scrubber_rating(context[:example_input], 4) == 10
+  end
+
+  test "part 2 - works for given example", context do
+    assert Day3.life_support_rating(context[:example_input], 4) == 230
+  end
+
+  test "part 2 - solves the puzzle", context do
+    assert Day3.life_support_rating(context[:puzzle_input], 11) == 1_007_985
   end
 end
